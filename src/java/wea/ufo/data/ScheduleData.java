8c0505cbdaf417d8e9c3a37c8b83738a7f1262ca
Christoph Wurst <christoph@winzerhof-wurst.at>
2016-01-24 00:34:37 +0100

    remove unused import

--- src/java/wea/ufo/data/ScheduleData.java
+++ src/java/wea/ufo/data/ScheduleData.java
@@ -9,7 +9,6 @@ import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
 import javax.annotation.PostConstruct;
 import javax.faces.application.Application;
 import javax.faces.view.ViewScoped;
